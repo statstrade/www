2721ec1e19f54cc736114d0ec11a9896bf1f3b85
Chris Zheng <zcaudate@outlook.com>
2023-05-18 00:56:55 +0800

    adding custom files

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "mode":"jit",
+  "content":["./pages/**/*.{js,ts,jsx,tsx}"],
+  "daisyui":{
+    "styled":true,
+    "rtl":false,
+    "themes":[
+      {
+      "statstrade":{
+        "success":"#38e8b6",
+        "base-100":"#171212",
+        "neutral":"#110E0E",
+        "warning":"#FBBD23",
+        "secondary":"#5f4abd",
+        "info":"#aa97fc",
+        "error":"#F87272",
+        "primary":"#38e8b6",
+        "accent":"#aaaae0"
+      }
+    }
+    ]
+  },
+  "plugins":[require("daisyui")]
+}
